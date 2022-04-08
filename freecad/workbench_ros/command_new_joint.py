7e6f3f346cf71e7b8250873eb9479d29ea3c7d26
Gaël Écorchard <gael.ecorchard@cvut.cz>
2022-04-08 07:53:28 +0200

    Add the NewJoint command

---
+++ freecad/workbench_ros/command_new_joint.py
@@ -0,0 +1,30 @@
+import FreeCAD as fc
+
+import FreeCADGui as fcgui
+
+from PySide import QtCore  # FreeCAD's PySide!
+
+from .utils import ICON_PATH
+from .utils import is_robot_selected
+
+
+class _NewJointCommand:
+    """The command definition to create a new Joint object."""
+    def GetResources(self):
+        return {'Pixmap': str(ICON_PATH.joinpath('ros_9dotslogo_color.svg')),
+                'MenuText': QtCore.QT_TRANSLATE_NOOP("workbench_ros", 'New Joint'),
+                'Accel': 'N, L',
+                'ToolTip': QtCore.QT_TRANSLATE_NOOP('workbench_ros', 'Create a Joint.')}
+
+    def IsActive(self):
+        return is_robot_selected()
+
+    def Activated(self):
+        fc.activeDocument().openTransaction('Create Joint')
+        fcgui.doCommand('')
+        fcgui.addModule('freecad.workbench_ros.joint')
+        fcgui.doCommand("_joint = freecad.workbench_ros.joint.makeJoint('Joint')")
+        fcgui.doCommand('FreeCADGui.ActiveDocument.setEdit(_joint.Name)')
+
+
+fcgui.addCommand('NewJoint', _NewJointCommand())

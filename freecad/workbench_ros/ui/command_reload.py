da4ef8a0fe035aa4073a6d429b37863bc09e2482
Gaël Écorchard <gael.ecorchard@cvut.cz>
2023-04-15 20:48:47 +0200

    Improve reload

--- freecad/workbench_ros/ui/command_reload.py
+++ freecad/workbench_ros/ui/command_reload.py
@@ -19,27 +19,33 @@ class _ReloadCommand:
 
     def Activated(self):
         def _reload_module(module_name: str) -> None:
-            module = import_module(module_name)
+            try:
+                module = import_module(module_name)
+            except ImportError:
+                return
             reload(module)
         _reload_module('freecad.workbench_ros.assembly4_utils')
         _reload_module('freecad.workbench_ros.assembly_from_urdf')
         _reload_module('freecad.workbench_ros.coin_utils')
         _reload_module('freecad.workbench_ros.deep_copy')
-        _reload_module('freecad.workbench_ros.freecadgui_utils')
         _reload_module('freecad.workbench_ros.freecad_utils')
+        _reload_module('freecad.workbench_ros.freecadgui_utils')
         _reload_module('freecad.workbench_ros.gui_utils')
         _reload_module('freecad.workbench_ros.import_dae')
         _reload_module('freecad.workbench_ros.joint')
         _reload_module('freecad.workbench_ros.link')
         _reload_module('freecad.workbench_ros.mesh_utils')
         _reload_module('freecad.workbench_ros.placement_utils')
-        _reload_module('freecad.workbench_ros.robot_from_urdf')
         _reload_module('freecad.workbench_ros.robot')
+        _reload_module('freecad.workbench_ros.robot_from_urdf')
         _reload_module('freecad.workbench_ros.ros_utils')
+        _reload_module('freecad.workbench_ros.urdf_loader')
+        _reload_module('freecad.workbench_ros.urdf_parser_utils')
         _reload_module('freecad.workbench_ros.urdf_utils')
         _reload_module('freecad.workbench_ros.utils')
         _reload_module('freecad.workbench_ros.version')
         _reload_module('freecad.workbench_ros.wb_utils')
+        _reload_module('freecad.workbench_ros.xacro_loader')
         _reload_module('freecad.workbench_ros.xacro_object')
         # These ones are probably not working (no effect because workbench
         # already created).
@@ -54,10 +60,6 @@ class _ReloadCommand:
         _reload_module('freecad.workbench_ros.ui.command_set_link_mounted_placement')
         _reload_module('freecad.workbench_ros.ui.command_sphere_from_bounding_box')
         _reload_module('freecad.workbench_ros.ui.command_urdf_export')
-        # Modules that may fail due to missing ROS modules.
-        # freecad.workbench_ros.urdf_parser_utils
-        # freecad.workbench_ros.xacro_loader
-        # freecad.workbench_ros.urdf_loader
 
 
 fcgui.addCommand('Reload', _ReloadCommand())

effdb4b58fc78ce06fba928b76c816a7d9d85a55
Gaël Écorchard <gael.ecorchard@cvut.cz>
2023-05-04 23:51:35 +0200

    Simplify code and improve label for box from bbox

--- freecad/cross/ui/command_box_from_bounding_box.py
+++ freecad/cross/ui/command_box_from_bounding_box.py
@@ -3,6 +3,7 @@ import FreeCADGui as fcgui
 
 from ..freecad_utils import error
 from ..freecad_utils import warn
+from ..freecad_utils import strip_subelement
 from ..gui_utils import tr
 from ..placement_utils import get_global_placement
 
@@ -28,23 +29,19 @@ class BoxFromBoundingBoxCommand:
                 # Inside a part.
                 subpath = selection_object.SubElementNames[0]
                 try:
-                    # Normal Shape.
-                    subobj = obj.getSubObjectList(subpath)[-1].Shape
+                    subobj = obj.getSubObjectList(subpath)[-1].getPropertyOfGeometry()
                 except AttributeError:
-                    # Mesh.
-                    subobj = obj.getSubObjectList(subpath)[-1].Mesh
+                    is_one_object_incompatible = True
+                    continue
                 placement = get_global_placement(obj, subpath)
                 # Cancel the rotation because bounding boxes are axis-aligned.
                 placement.Rotation = fc.Rotation()
-                box_name = obj.Label + selection_object.SubElementNames[0] + '_bbox'
+                box_name = obj.Label + '.' + strip_subelement(subpath) + '_bbox'
             else:
                 # Outside of any part.
-                # TODO: simplify with subobj.getPropertyOfGeometry()?
-                if hasattr(obj, 'Shape'):
-                    subobj = obj.Shape
-                elif hasattr(obj, 'Mesh'):
-                    subobj = obj.Mesh
-                else:
+                try:
+                    subobj = obj.getPropertyOfGeometry()
+                except AttributeError:
                     is_one_object_incompatible = True
                     continue
                 box_name = obj.Label + '_bbox'
@@ -55,6 +52,7 @@ class BoxFromBoundingBoxCommand:
             bbox = subobj.BoundBox
             doc.openTransaction(tr('Box from bounding box'))
             box = doc.addObject('Part::Box', box_name)
+            box.Label = box_name
             box.Length = bbox.XLength
             box.Width = bbox.YLength
             box.Height = bbox.ZLength

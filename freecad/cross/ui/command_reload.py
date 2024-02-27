89064da1eb3eba013afde11c8f8262e4ff4d4a91
Yang Hongbo <yanghongbo@iotpi.io>
2024-02-27 11:26:10 +0800

    added reloading robot_proxy

--- freecad/cross/ui/command_reload.py
+++ freecad/cross/ui/command_reload.py
@@ -41,6 +41,7 @@ class _ReloadCommand:
         _reload_module('freecad.cross.planning_scene_proxy')
         _reload_module('freecad.cross.planning_scene_utils')
         _reload_module('freecad.cross.robot')
+        _reload_module('freecad.cross.robot_proxy')
         _reload_module('freecad.cross.robot_from_urdf')
         _reload_module('freecad.cross.ros.node')
         _reload_module('freecad.cross.ros.planning_scene')

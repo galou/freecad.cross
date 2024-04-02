81a14c9b4a473508fb2fb0b3dc98f8f23ded6604
menrise <gutskombayner@gmail.com>
2024-04-02 19:39:05 +0300

    add debug init

--- freecad/cross/__init__.py
+++ freecad/cross/__init__.py
@@ -11,6 +11,18 @@ add_ros_library_path(g_ros_distro)
 # Must be imported after the call to `add_ros_library_path`.
 from .ros.utils import is_ros_found  # noqa: E402.
 
+import os
+
+# init debug for VS Code
+if os.environ.get('FREECADCROSS_DEBUG'):
+    # how to use:
+    # FREECADCROSS_DEBUG=1 command_to_run_freecad
+    # Dont forget install debugpy if set debug env
+    import debugpy
+    debugpy.configure(python="python3")
+    debugpy.listen(5678)
+    debugpy.trace_this_thread(True)
+    debugpy.debug_this_thread()
 
 if is_ros_found():
     fc.addImportType('URDF files (*.urdf *.xacro)', 'import_urdf')
